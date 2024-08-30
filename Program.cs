1e8e4b3f4fd1c580cab0aa91f4859c48da9327ea
Derit7042 <junsu6752@gmail.com>
2024-08-30 22:43:44 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DoIt_Cshop_Start_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
